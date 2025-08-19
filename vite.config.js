c919e694c2d718bd090e454d6a2b1237b1836b63
coltosiohann <coltosiohann@gmail.com>
2025-08-19 16:10:29 +0300

    Update vercel routing config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/20max-web/',  // Changed from /20max-website/
+  base: '/',  // Changed from /20max-website/
   css: {
     postcss: './postcss.config.js',
   },
